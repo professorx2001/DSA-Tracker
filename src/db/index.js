6e8cb4a6b5ceb48862347e0a67e49ce64617a828
Md Zaki Hussain <mdzakihusain@gmail.com>
2025-03-08 19:42:31 +0530

    set up Express server and MongoDB connection

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants.js";
+const connectDB = async () => {
+    try {
+        
+        const connectionInstance = await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected: ${connectionInstance.connection.port}`);
+    } catch (error) {
+        console.error(`MongoDB Connection Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+import 'dotenv/config';
+import app from "./app.js";
+import connectDB from "./db/index.js";
+
+const port = process.env.PORT || 5000;
+
+
+connectDB()
+.then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})
+.catch((error) => {
+  console.log(`MongoDB connection error. ${error.message}`);
+});
